f1180a5fa83b360b0cb6b8706079e7a1dcdcfd54
Brandon Parmenter <leftiness@gmail.com>
2018-06-09 15:39:46 -0400

    Issue #1: Begin wrangling hotkeys

---
+++ awesome/key.lua
@@ -0,0 +1,129 @@
+local awful = require('awful')
+local beautiful = require('beautiful')
+local hotkeys = require('awful.hotkeys_popup').widget
+
+local Mod = require('mod')
+local Mouse = require('mouse')
+
+local SIGNAL_MOUSE_ENTER = 'mouse::enter'
+local SIGNAL_FOCUS = 'focus'
+local SIGNAL_UNFOCUS = 'unfocus'
+
+local Client = {}
+
+function Client.next() awful.client.focus.byidx(1) end
+function Client.prev() awful.client.focus.byidx(-1) end
+
+function Client.fullscreen(c) c.fullscreen = not c.fullscreen; c:raise() end
+function Client.maximize(c) c.maximized = not c.maximized; c:raise() end
+function Client.focus(c) client.focus = c; c:raise() end
+function Client.promote(c) c:swap(awful.client.getmaster()) end
+function Client.kill(c) c:kill() end
+
+function Client.border_focus(c) c.border_color = beautiful.border_focus end
+function Client.border_normal(c) c.border_color = beautiful.border_normal end
+
+function Client.to_tag(i)
+  return function()
+    if client.focus then
+      client.focus:move_to_tag(client.focus.screen.tags[i])
+    end
+  end
+end
+
+local Master = {}
+function Master.grow() awful.tag.incmwfact(0.05) end
+function Master.shrink() awful.tag.incmwfact(-0.05) end
+
+local Prompt = {}
+function Prompt.run() awful.screen.focused().mypromptbox:run() end
+
+local Tag = {}
+function Tag.view(i)
+  return function()
+    awful.screen.focused().tags[i]:view_only()
+  end
+end
+
+local Tip = {
+  Awesome = {
+    SHOW_HELP = { description = 'show help', group = 'awesome' },
+    RELOAD =    { description = 'reload',    group = 'awesome' },
+    QUIT =      { description = 'quit',      group = 'awesome' }
+  },
+  Tag = {
+    PREVIOUS = { description = 'previous', group = 'tag' },
+    NEXT =     { description = 'next',     group = 'tag' }
+  },
+  Client = {
+    NEXT =     { description = 'next',     group = 'client' },
+    PREVIOUS = { description = 'previous', group = 'client' },
+    to_tag = function(i)
+      return { description = 'move client to tag #'..i, group = 'client' }
+    end,
+    FULLSCREEN = { description = 'fullscreen',  group = 'client' },
+    MAXIMIZE =   { description = 'maximize',    group = 'client' },
+    PROMOTE =    { description = 'make master', group = 'client' },
+    CLOSE =      { description = 'close',       group = 'client' }
+  },
+  Layout = {
+    GROW =   { description = 'grow master',   group = 'layout' },
+    SHRINK = { description = 'shrink master', group = 'layout' }
+  },
+  Prompt = {
+    RUN = { description = 'run prompt', group = 'launcher' }
+  },
+  Tag = {
+    view = function(i)
+      return { description = 'view #'..i, group = 'tag' }
+    end
+  }
+}
+
+local Key = {}
+
+Key.GLOBAL = awful.util.table.join(
+  awful.key(Mod.SUPER_SHIFT, 's', hotkeys.show_help, Tip.Awesome.SHOW_HELP),
+  awful.key(Mod.SUPER_SHIFT, 'r', awesome.restart,   Tip.Awesome.RELOAD),
+  awful.key(Mod.SUPER_SHIFT, 'q', awesome.quit,      Tip.Awesome.QUIT),
+
+  awful.key(Mod.SUPER_SHIFT, 'h', awful.tag.viewprev, Tip.Tag.PREVIOUS),
+  awful.key(Mod.SUPER_SHIFT, 'l', awful.tag.viewnext, Tip.Tag.NEXT),
+
+  awful.key(Mod.SUPER, 'j', Client.next, Tip.Client.NEXT),
+  awful.key(Mod.SUPER, 'k', Client.prev, Tip.Client.PREVIOUS),
+
+  awful.key(Mod.SUPER, 'l', Master.grow,   Tip.Layout.GROW),
+  awful.key(Mod.SUPER, 'h', Master.shrink, Tip.Layout.SHRINK),
+
+  awful.key(Mod.SUPER, 'space', Prompt.run, Tip.Prompt.RUN)
+)
+
+for i = 1, 9 do
+  Key.GLOBAL = awful.util.table.join(
+    Key.GLOBAL,
+    awful.key(Mod.SUPER, '#'..i+9, Tag.view(i), Tip.Tag.view(i)),
+
+    awful.key(Mod.SUPER_SHIFT, '#'..i+9, Client.to_tag(i), Tip.Client.to_tag(i))
+  )
+end
+
+Key.CLIENT = awful.util.table.join(
+  awful.key(Mod.SUPER, 'f', Client.fullscreen, Tip.Client.FULLSCREEN),
+  awful.key(Mod.SUPER, 'm', Client.maximize,   Tip.Client.MAXIMIZE),
+
+  awful.key(Mod.SUPER, 'Return', Client.promote, Tip.Client.PROMOTE),
+
+  awful.key(Mod.SUPER_SHIFT, 'c', Client.kill, Tip.Client.CLOSE)
+)
+
+Key.CLIENT_BUTTON = awful.util.table.join(
+  awful.button({},        Mouse.click.LEFT, Client.focus),
+  awful.button(Mod.SUPER, Mouse.click.LEFT, awful.mouse.client.move)
+)
+
+client.connect_signal(SIGNAL_MOUSE_ENTER, Client.focus)
+client.connect_signal(SIGNAL_FOCUS, Client.border_focus)
+client.connect_signal(SIGNAL_UNFOCUS, Client.border_normal)
+
+return Key
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -4,10 +4,8 @@ local awful = require('awful')
 require('awful.autofocus')
 local beautiful = require('beautiful')
 local naughty = require('naughty')
-local hotkeys_popup = require('awful.hotkeys_popup').widget
 
-local Mod = require('mod')
-local Mouse = require('mouse')
+local Key = require('key')
 local Runtime = require('runtime')
 local Screen = require('screen')
 
@@ -26,136 +24,8 @@ os.execute(CMD_NITROGEN)
 awful.layout.layouts = { awful.layout.suit.tile }
 awful.screen.connect_for_each_screen(Screen.on_connect)
 
--- {{{ Key bindings
-globalkeys = awful.util.table.join(
-  awful.key(
-    Mod.SUPER,
-    's',
-    hotkeys_popup.show_help,
-    { description = 'show help', group = 'awesome' }
-  ),
-  awful.key(
-    Mod.SUPER_SHIFT,
-    'h',
-    awful.tag.viewprev,
-    { description = 'view previous', group = 'tag' }
-  ),
-  awful.key(
-    Mod.SUPER_SHIFT,
-    'l',
-    awful.tag.viewnext,
-    { description = 'view next', group = 'tag'}
-  ),
-  awful.key(
-    Mod.SUPER,
-    'j',
-    function () awful.client.focus.byidx(1) end,
-    { description = 'focus next by index', group = 'client' }
-  ),
-  awful.key(
-    Mod.SUPER,
-    'k',
-    function () awful.client.focus.byidx(-1) end,
-    { description = 'focus previous by index', group = 'client' }
-  ),
-  awful.key(
-    Mod.SUPER_SHIFT,
-    'r',
-    awesome.restart,
-    { description = 'reload awesome', group = 'awesome' }
-  ),
-  awful.key(
-    Mod.SUPER_SHIFT,
-    'q',
-    awesome.quit,
-    { description = 'quit awesome', group = 'awesome' }
-  ),
-  awful.key(
-    Mod.SUPER,
-    'l',
-    function () awful.tag.incmwfact(0.05) end,
-    { description = 'increase master width factor', group = 'layout' }
-  ),
-  awful.key(
-    Mod.SUPER,
-    'h',
-    function () awful.tag.incmwfact(-0.05) end,
-    { description = 'decrease master width factor', group = 'layout' }
-  ),
-  awful.key(
-    Mod.SUPER,
-    'space',
-    function () awful.screen.focused().mypromptbox:run() end,
-    { description = 'run prompt', group = 'launcher' }
-  )
-)
+root.keys(Key.GLOBAL)
 
-clientkeys = awful.util.table.join(
-  awful.key(
-    Mod.SUPER,
-    'f',
-    function (c) c.fullscreen = not c.fullscreen; c:raise() end,
-    { description = 'toggle fullscreen', group = 'client' }
-  ),
-  awful.key(
-    Mod.SUPER_SHIFT,
-    'c',
-    function (c) c:kill() end,
-    { description = 'close', group = 'client' }
-  ),
-  awful.key(
-    Mod.SUPER,
-    'Return',
-    function (c) c:swap(awful.client.getmaster()) end,
-    { description = 'move to master', group = 'client' }
-  ),
-  awful.key(
-    Mod.SUPER,
-    'm',
-    function (c) c.maximized = not c.maximized; c:raise() end,
-    { description = 'maximize', group = 'client' }
-  )
-)
-
--- Bind all key numbers to tags.
-for i = 1, 9 do
-  globalkeys = awful.util.table.join(
-    globalkeys,
-    awful.key(
-      Mod.SUPER,
-      '#' .. i + 9,
-      function() awful.screen.focused().tags[i]:view_only() end,
-      { description = 'view tag #'..i, group = 'tag' }
-    ),
-    awful.key(
-      Mod.SUPER_SHIFT,
-      '#' .. i + 9,
-      function()
-        if client.focus then
-          client.focus:move_to_tag(client.focus.screen.tags[i])
-        end
-      end,
-      { description = 'move focused client to tag #'..i, group = 'tag' }
-    )
-  )
-end
-
-clientbuttons = awful.util.table.join(
-  awful.button(
-    {},
-    Mouse.click.LEFT,
-    function (c) client.focus = c; c:raise() end
-  ),
-  awful.button(Mod.SUPER, Mouse.click.LEFT, awful.mouse.client.move)
-)
-
--- Set keys
-root.keys(globalkeys)
--- }}}
-
--- {{{ Rules
-
--- Rules to apply to new clients (through the 'manage' signal).
 awful.rules.rules = {
   {
     rule = {},
@@ -164,32 +34,11 @@ awful.rules.rules = {
       border_color = beautiful.border_normal,
       focus = awful.client.focus.filter,
       raise = true,
-      keys = clientkeys,
-      buttons = clientbuttons,
+      keys = Key.CLIENT,
+      buttons = Key.CLIENT_BUTTON,
       screen = awful.screen.preferred,
       placement = awful.placement.no_overlap+awful.placement.no_offscreen,
       size_hints_honor = false
     }
-  },
-
+  }
 }
--- }}}
-
--- {{{ Signals
-
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal('mouse::enter', function (c) client.focus = c end)
-
--- Set border color on focus
-client.connect_signal(
-  'focus',
-  function (c) c.border_color = beautiful.border_focus end
-)
-
--- Set border color on unfocus
-client.connect_signal(
-  'unfocus',
-  function (c) c.border_color = beautiful.border_normal end
-)
-
--- }}}
